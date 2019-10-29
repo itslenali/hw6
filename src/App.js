82e6914c31d51eb7657c25c40655b93127a50860
Lena Li <lenali@Lenas-MBP.fios-router.home>
2019-10-28 20:03:16 -0400

    finally. made second slider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './assets/seattle.jpg';
 
 
 class App extends React.Component {
@@ -12,11 +11,18 @@ class App extends React.Component {
     }
 
     this.handleStart = this.handleStart.bind(this);
+    
   }
 
   handleStart(e) {
     this.setState({started: true})
   }
+  
+  handleClick(e) {
+    this.onClick = this.handleClick.bind(this);
+    console.log("hello");
+  }
+
 
   render() {
     const { started } = this.state;
@@ -24,62 +30,127 @@ class App extends React.Component {
       <div className="App">
         <div className="App-bg">
           <div className={`${started ? "grow" : ""} home-rect`} onClick={this.handleStart}>
-            { !started ? (<div>
+            { !started ? (
+            
+            <div>
               <h1>My Place</h1>
               <h3>The Pacific Northwest</h3>
               </div>
             ) :
+            
+            
             (
+            <div id="sectionFirst" className={`${started ? "animatePhotos" : ""} home-rect`} onClick={this.handleStart}>
              <div className="main-container">
-              <div className="main-text-container">
-                <h1>My Place</h1>
-                <div className="photos-container">
-                  <ul>
-                  <img src="https://images.unsplash.com/photo-1438401171849-74ac270044ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2384&q=80" className="preview-img" alt="seattle"/>   
-                  </ul>
+                  <div className="main-text-container">
+                    <h1>My Place</h1>
+                    <div class="photos-container">
+                      <ul>
+                      <img src="https://images.unsplash.com/photo-1438401171849-74ac270044ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2384&q=80"  className="preview-img" onClick={(e) => this.handleClick(e)} alt="seattle"/>   
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="main-text-container">
+                    <h1>My Place</h1>
+                    <div className="photos-container">
+                      <ul>
+                      <img src="https://images.unsplash.com/photo-1438401171849-74ac270044ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2384&q=80" className="preview-img" alt="seattle"/>   
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="main-text-container">
+                    <h1>My Place</h1>
+                    <div className="photos-container">
+                      <ul>
+                      <img src="https://images.unsplash.com/photo-1438401171849-74ac270044ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2384&q=80" className="preview-img" alt="seattle"/>   
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="main-text-container">
+                    <h1>My Place</h1>
+                    <div className="photos-container">
+                      <ul>
+                      <img src="https://images.unsplash.com/photo-1438401171849-74ac270044ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2384&q=80" className="preview-img" alt="seattle"/>   
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="main-text-container">
+                    <h1>My Place</h1>
+                    <div className="photos-container">
+                      <ul>
+                      <img src="https://images.unsplash.com/photo-1438401171849-74ac270044ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2384&q=80" className="preview-img" alt="seattle"/>   
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="main-text-container">
+                    <h1>My Place</h1>
+                    <div className="photos-container">
+                      <ul>
+                      <img src="https://images.unsplash.com/photo-1438401171849-74ac270044ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2384&q=80" className="preview-img" alt="seattle"/>   
+                      </ul>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div className="main-text-container">
-                <h1>My Place</h1>
-                <div className="photos-container">
-                  <ul>
-                  <img src="https://images.unsplash.com/photo-1438401171849-74ac270044ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2384&q=80" className="preview-img" alt="seattle"/>   
-                  </ul>
+
+                <div id="pagebreaks"></div>
+                
+                <div className="new-page-container" >
+                  <div className="second-text-container">
+                    <h1>My Place</h1>
+                    <div class="photos-container">
+                      <ul>
+                      <img src="https://images.unsplash.com/photo-1438401171849-74ac270044ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2384&q=80" class="img" className="main-img second-text-container" onClick={(e) => this.handleClick(e)} />   
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="second-text-container">
+                    <h1>My Place</h1>
+                    <div class="photos-container">
+                      <ul>
+                      <img src="https://images.unsplash.com/photo-1438401171849-74ac270044ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2384&q=80" class="img" className="main-img second-text-container" onClick={(e) => this.handleClick(e)} />   
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="second-text-container">
+                    <h1>My Place</h1>
+                    <div class="photos-container">
+                      <ul>
+                      <img src="https://images.unsplash.com/photo-1438401171849-74ac270044ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2384&q=80" class="img" className="main-img second-text-container" onClick={(e) => this.handleClick(e)} />   
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="second-text-container">
+                    <h1>My Place</h1>
+                    <div class="photos-container">
+                      <ul>
+                      <img src="https://images.unsplash.com/photo-1438401171849-74ac270044ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2384&q=80" class="img" className="main-img second-text-container" onClick={(e) => this.handleClick(e)} />   
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="second-text-container">
+                    <h1>My Place</h1>
+                    <div class="photos-container">
+                      <ul>
+                      <img src="https://images.unsplash.com/photo-1438401171849-74ac270044ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2384&q=80" class="img" className="main-img second-text-container" onClick={(e) => this.handleClick(e)} />   
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="second-text-container">
+                    <h1>My Place</h1>
+                    <div class="photos-container">
+                      <ul>
+                      <img src="https://images.unsplash.com/photo-1438401171849-74ac270044ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2384&q=80" class="img" className="main-img second-text-container" onClick={(e) => this.handleClick(e)} />   
+                      </ul>
+                    </div>
+                  </div>
+           
+                
                 </div>
+              
+                <div id="pagebreaks"></div>
+
               </div>
-              <div className="main-text-container">
-                <h1>My Place</h1>
-                <div className="photos-container">
-                  <ul>
-                  <img src="https://images.unsplash.com/photo-1438401171849-74ac270044ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2384&q=80" className="preview-img" alt="seattle"/>   
-                  </ul>
-                </div>
-              </div>
-              <div className="main-text-container">
-                <h1>My Place</h1>
-                <div className="photos-container">
-                  <ul>
-                  <img src="https://images.unsplash.com/photo-1438401171849-74ac270044ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2384&q=80" className="preview-img" alt="seattle"/>   
-                  </ul>
-                </div>
-              </div>
-              <div className="main-text-container">
-                <h1>My Place</h1>
-                <div className="photos-container">
-                  <ul>
-                  <img src="https://images.unsplash.com/photo-1438401171849-74ac270044ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2384&q=80" className="preview-img" alt="seattle"/>   
-                  </ul>
-                </div>
-              </div>
-              <div className="main-text-container">
-                <h1>My Place</h1>
-                <div className="photos-container">
-                  <ul>
-                  <img src="https://images.unsplash.com/photo-1438401171849-74ac270044ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2384&q=80" className="preview-img" alt="seattle"/>   
-                  </ul>
-                </div>
-              </div>
-            </div> 
+             
             )
             }
           </div>
@@ -87,6 +158,10 @@ class App extends React.Component {
       </div>
     );
   }
+
+  
+
+
 }
 
 
